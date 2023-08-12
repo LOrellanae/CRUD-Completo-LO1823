cfb4ba612ab78e8451bdfd08d3371dc83545cd91
LOrellanae <lorellanae2@miumg.edu.gt>
2023-08-11 21:08:56 -0600

    Crud Completo v3

--- Js/CallAjax.js
+++ Js/CallAjax.js
@@ -8,9 +8,9 @@ function llamadaDemo() {
     /*Creating an HTML table dynamically
    https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Traversing_an_HTML_table_with_JavaScript_and_DOM_Interfaces#creating_an_html_table_dynamically*/
     //uso de Fetch https://developer.mozilla.org/es/docs/Web/API/Fetch_API/Using_Fetch
-   
-        var url = "https://movie.azurewebsites.net/api/cartelera?title=&ubication=";
-        
+
+    var url = "https://movie.azurewebsites.net/api/cartelera?title=&ubication=";
+
 
 
     fetch(url)
@@ -19,14 +19,14 @@ function llamadaDemo() {
             console.log(data)
             var divPeli = document.getElementById('divPeli');
             divPeli.innerHTML = '';
-            
+
             if (data.length > 0) {
                 //alert(data.status );
                 for (let index = 0; index < data.length; index++) {
 
                     const div = document.createElement("div");
                     div.className = "col-md-6";
-               
+
 
                     const div2 = document.createElement("div");
                     div2.className = "row g-0 border rounded overflow-hidden flex-md-row mb-1 shadow-sm h-md-250 position-relative";
@@ -34,7 +34,7 @@ function llamadaDemo() {
                     /*Info Peli*/
                     const div3 = document.createElement("div");
                     div3.className = "col p-4 d-flex flex-column position-static";
-                    
+
                     const types = document.createElement("strong");
                     types.className = "d-inline-block mb-2 text-primary";
                     types.innerText = data[index].Type;
@@ -43,30 +43,30 @@ function llamadaDemo() {
                     // imID.className = "d-inline-block mb-1 text-primary";
                     // imID.innerText = data[index].imdbID;
 
-                    const div5= document.createElement("div");
+                    const div5 = document.createElement("div");
                     div5.className = "btn-group col-md-12 mb-4 btn-group";
-                    div5.role="group";
+                    div5.role = "group";
 
 
-                    const buttonActualiza=document.createElement("button");
-                    buttonActualiza.type="button";
-                    buttonActualiza.className="btn btn-success";
-                    buttonActualiza.textContent="Actualizar";
+                    const buttonActualiza = document.createElement("button");
+                    buttonActualiza.type = "button";
+                    buttonActualiza.className = "btn btn-success";
+                    buttonActualiza.textContent = "Actualizar";
                     buttonActualiza.setAttribute('data-bs-toggle', 'modal');
-                    buttonActualiza.setAttribute('data-bs-target','#MdlForm2');
-                    buttonActualiza.addEventListener('click',llamada5,false);
-                    buttonActualiza.id=data[index].imdbID;
+                    buttonActualiza.setAttribute('data-bs-target', '#MdlForm2');
+                    buttonActualiza.addEventListener('click', llamada5, false);
+                    buttonActualiza.id = data[index].imdbID;
                     div5.appendChild(buttonActualiza);
 
-                    const buttonEliminar=document.createElement("button");
-                    buttonEliminar.type="button";
-                    buttonEliminar.className="btn btn-danger";
-                    buttonEliminar.textContent="Eliminar";
-                    buttonEliminar.id=data[index].imdbID;
-                    buttonEliminar.addEventListener('click',llamada3,false);
+                    const buttonEliminar = document.createElement("button");
+                    buttonEliminar.type = "button";
+                    buttonEliminar.className = "btn btn-danger";
+                    buttonEliminar.textContent = "Eliminar";
+                    buttonEliminar.id = data[index].imdbID;
+                    buttonEliminar.addEventListener('click', llamada3, false);
                     div5.appendChild(buttonEliminar);
 
-                  
+
 
                     const title = document.createElement("h3");
                     title.className = "mb-0";
@@ -75,17 +75,17 @@ function llamadaDemo() {
                     const anio = document.createElement("div");
                     anio.className = "mb-1 text-body-secondary";
                     anio.innerText = data[index].Year;
-                    
+
                     const description = document.createElement("p");
                     description.className = "card-text mb-auto";
-                  if  (data[index].description === undefined ||  data[index].description === null) {
+                    if (data[index].description === undefined || data[index].description === null) {
 
-                    description.innerText = "";
+                        description.innerText = "";
 
-                  }else{
-                    description.innerText = data[index].description.substring(0,150) + " ...";
-                  }
-                
+                    } else {
+                        description.innerText = data[index].description.substring(0, 150) + " ...";
+                    }
+
 
                     const ubitacion = document.createElement("a");
                     ubitacion.className = "stretched-link";
@@ -97,7 +97,7 @@ function llamadaDemo() {
                     div3.appendChild(anio);
                     div3.appendChild(description);
                     div3.appendChild(ubitacion);
-                 
+
 
                     const div4 = document.createElement("div");
                     div4.className = "col-auto d-none d-lg-block";
@@ -116,20 +116,20 @@ function llamadaDemo() {
 }
 
 /*Llamada por ID*/
-function llamada1(){
+function llamada1() {
     //alert("llamada 1");
     var titulo = document.getElementById("txtPeli");
     var ubicacion = document.getElementById("cmbUbicacion");
 
 
-var url="";
+    var url = "";
 
-    
-    if (titulo.value != null && ubicacion.value != null){
-         url = "https://movie.azurewebsites.net/api/cartelera?title="+titulo.value+"&ubication="+(ubicacion.value=="Mostrar Todos"?"":ubicacion.value)+"";
+
+    if (titulo.value != null && ubicacion.value != null) {
+        url = "https://movie.azurewebsites.net/api/cartelera?title=" + titulo.value + "&ubication=" + (ubicacion.value == "Mostrar Todos" ? "" : ubicacion.value) + "";
     }
-    else{
-     url = "https://movie.azurewebsites.net/api/cartelera?title=&ubication=";
+    else {
+        url = "https://movie.azurewebsites.net/api/cartelera?title=&ubication=";
     }
 
     alert(url)
@@ -139,14 +139,14 @@ var url="";
             console.log(data)
             var divPeli = document.getElementById('divPeli');
             divPeli.innerHTML = '';
-            
+
             if (data.length > 0) {
                 //alert(data.status );
                 for (let index = 0; index < data.length; index++) {
 
                     const div = document.createElement("div");
                     div.className = "col-md-6";
-                    
+
 
                     const div2 = document.createElement("div");
                     div2.className = "row g-0 border rounded overflow-hidden flex-md-row mb-1 shadow-sm h-md-250 position-relative";
@@ -154,7 +154,7 @@ var url="";
                     /*Info Peli*/
                     const div3 = document.createElement("div");
                     div3.className = "col p-4 d-flex flex-column position-static";
-                    
+
                     const types = document.createElement("strong");
                     types.className = "d-inline-block mb-2 text-primary";
                     types.innerText = data[index].Type;
@@ -163,29 +163,29 @@ var url="";
                     // imID.className = "d-inline-block mb-1 text-primary";
                     // imID.innerText = data[index].imdbID;
 
-                    const div5= document.createElement("div");
+                    const div5 = document.createElement("div");
                     div5.className = "btn-group col-md-12 mb-4 btn-group";
-                    div5.role="group";
+                    div5.role = "group";
 
-                    const buttonActualiza=document.createElement("button");
-                    buttonActualiza.type="button";
-                    buttonActualiza.className="btn btn-success";
-                    buttonActualiza.textContent="Actualizar";
+                    const buttonActualiza = document.createElement("button");
+                    buttonActualiza.type = "button";
+                    buttonActualiza.className = "btn btn-success";
+                    buttonActualiza.textContent = "Actualizar";
                     buttonActualiza.setAttribute('data-bs-toggle', 'modal');
-                    buttonActualiza.setAttribute('data-bs-target','#MdlForm2');
-                    buttonActualiza.addEventListener('click',llamada5,false);
-                    buttonActualiza.id=data[index].imdbID;
+                    buttonActualiza.setAttribute('data-bs-target', '#MdlForm2');
+                    buttonActualiza.addEventListener('click', llamada5, false);
+                    buttonActualiza.id = data[index].imdbID;
                     div5.appendChild(buttonActualiza);
 
-                    const buttonEliminar=document.createElement("button");
-                    buttonEliminar.type="button";
-                    buttonEliminar.className="btn btn-danger";
-                    buttonEliminar.textContent="Eliminar";
-                    buttonEliminar.addEventListener('click',llamada3,false);
-                    buttonEliminar.id=data[index].imdbID;
+                    const buttonEliminar = document.createElement("button");
+                    buttonEliminar.type = "button";
+                    buttonEliminar.className = "btn btn-danger";
+                    buttonEliminar.textContent = "Eliminar";
+                    buttonEliminar.addEventListener('click', llamada3, false);
+                    buttonEliminar.id = data[index].imdbID;
                     div5.appendChild(buttonEliminar);
 
-                  
+
                     const title = document.createElement("h3");
                     title.className = "mb-0";
                     title.innerText = data[index].Title;
@@ -193,17 +193,17 @@ var url="";
                     const anio = document.createElement("div");
                     anio.className = "mb-1 text-body-secondary";
                     anio.innerText = data[index].Year;
-                    
+
                     const description = document.createElement("p");
                     description.className = "card-text mb-auto";
-                    if  (data[index].description === undefined ||  data[index].description === null) {
+                    if (data[index].description === undefined || data[index].description === null) {
 
                         description.innerText = "";
-    
-                      }else{
-                        description.innerText = data[index].description.substring(0,150) + " ...";
-                      }
-                
+
+                    } else {
+                        description.innerText = data[index].description.substring(0, 150) + " ...";
+                    }
+
 
                     const ubitacion = document.createElement("a");
                     ubitacion.className = "stretched-link";
@@ -215,7 +215,7 @@ var url="";
                     div3.appendChild(anio);
                     div3.appendChild(description);
                     div3.appendChild(ubitacion);
-                 
+
 
                     const div4 = document.createElement("div");
                     div4.className = "col-auto d-none d-lg-block";
@@ -234,7 +234,7 @@ var url="";
 }
 
 /*Llamada por grabacion */
-function llamada2(){
+function llamada2() {
     var id = document.getElementById("txtID");
     var titulo = document.getElementById("txtTitle");
     var year = document.getElementById("txtYear");
@@ -243,65 +243,65 @@ function llamada2(){
     var description = document.getElementById("txtDes");
     var ubicacion = document.getElementById("cmbUbiModal");
     var estado = document.getElementById("cmbEstadoModal");
-    
+
     const Create = {
         imdbID: id.value,
         Title: titulo.value,
         Year: year.value,
         Type: types.value,
-        Poster:poster.value,
+        Poster: poster.value,
         description: description.value,
-        Ubication:ubicacion.value,
-        Estado:Number(estado.value) ,
-        };
-        
-        const options = {
+        Ubication: ubicacion.value,
+        Estado: Number(estado.value),
+    };
+
+    const options = {
         method: 'POST',
         headers: {
-        'Content-Type': 'application/json',
+            'Content-Type': 'application/json',
         },
         body: JSON.stringify(Create),
-        };
+    };
 
-        fetch('https://movie.azurewebsites.net/api/cartelera', options)
+    fetch('https://movie.azurewebsites.net/api/cartelera', options)
         .then(data => {
-      if (!data.ok) {
-        throw Error(data.status);
-       }
-       return data.json();
-      }).then(Create => {
-      alert(Create);
-     
-      }).catch(e => {
-      console.log(e);
-      });
-      document.querySelector('#AggPeli').reset();
+            if (!data.ok) {
+                throw Error(data.status);
+            }
+            return data.json();
+        }).then(Create => {
+            alert(Create);
+            llamadaDemo();
+        }).catch(e => {
+            console.log(e);
+        });
+    document.querySelector('#AggPeli').reset();
 }
 
-function llamada3(e){
+function llamada3(e) {
     const idpeli = e.target.id;
 
-    idpeli.disabled=true;
-    url = "https://movie.azurewebsites.net/api/cartelera?imdbID="+idpeli+"";
+    idpeli.disabled = true;
+    url = "https://movie.azurewebsites.net/api/cartelera?imdbID=" + idpeli + "";
     var options = {
         method: 'DELETE'
     };
 
     fetch(url, options)
         .then(response => response.json())
-        .then(data=>{
+        .then(data => {
 
-            alert("Codigo http: "+data.codError +" Mensaje: "+ data.msgRespuesta);
+            alert("Codigo http: " + data.codError + " Mensaje: " + data.msgRespuesta);
             llamadaDemo();
         })
         .catch(error => {
-            console.error('Error de red',error);
+            console.error('Error de red', error);
         });
-    
-        idpeli.disabled=false;     
+
+    idpeli.disabled = false;
 }
 
-function llamada4(){
+function llamada4() {
     // const idpeli = e.target.id;
     var id = document.getElementById("txtIDU");
     // id.innerText = idpeli;
@@ -312,46 +312,46 @@ function llamada4(){
     var description = document.getElementById("txtDesU");
     var ubicacion = document.getElementById("cmbUbiModalU");
     var estado = document.getElementById("cmbEstadoModalU");
-    
+
     const Create = {
         imdbID: id.value,
         Title: titulo.value,
         Year: year.value,
         Type: types.value,
-        Poster:poster.value,
+        Poster: poster.value,
         description: description.value,
-        Ubication:ubicacion.value,
-        Estado:Number(estado.value),
-        };
-        
-        const options = {
+        Ubication: ubicacion.value,
+        Estado: Number(estado.value),
+    };
+
+    const options = {
         method: 'PUT',
         headers: {
-        'Content-Type': 'application/json',
+            'Content-Type': 'application/json',
         },
         body: JSON.stringify(Create),
-        };
-        url = 'https://movie.azurewebsites.net/api/cartelera?imdbID='+id.value+'';
-        fetch(url, options)
+    };
+    url = 'https://movie.azurewebsites.net/api/cartelera?imdbID=' + id.value + '';
+    fetch(url, options)
         .then(data => {
-      if (!data.ok) {
-        throw Error(data.status);
-       }
-       return data.json();
-      }).then(data => {
-        alert("Codigo http: "+data.codError +" Mensaje: "+ data.msgRespuesta);
-        llamadaDemo();
-     
-      }).catch(e => {
-      console.log(e);
-      });
+            if (!data.ok) {
+                throw Error(data.status);
+            }
+            return data.json();
+        }).then(data => {
+            alert("Codigo http: " + data.codError + " Mensaje: " + data.msgRespuesta);
+            llamadaDemo();
+
+        }).catch(e => {
+            console.log(e);
+        });
 
 }
 
-function llamada5(e){
+function llamada5(e) {
     const idpeli = e.target.id;
     var id = document.getElementById("txtIDU");
-    if (id.value!=idpeli && id.value!=null) {
+    if (id.value != idpeli && id.value != null) {
         document.querySelector('#UPeli').reset();
     }
     id.value = idpeli;
@@ -364,18 +364,18 @@ function llamada5(e){
     var description = document.getElementById("txtDesU");
     var ubicacion = document.getElementById("cmbUbiModalU");
     var estado = document.getElementById("cmbEstadoModalU");
-    url = 'https://movie.azurewebsites.net/api/cartelera?imdbID='+idpeli;
+    url = 'https://movie.azurewebsites.net/api/cartelera?imdbID=' + idpeli;
     fetch(url)
         .then(response => response.json())
         .then(data => {
             console.log(data)
-            id.value=data.imdbID;
-            titulo.value=data.Title;
-            year.value=data.Year;
-            types.value=data.Type;
-            poster.value=data.Poster;
-            description.value=data.description;
-            ubicacion.value=data.Ubication;
-            estado.value=Number(data.Estado);
+            id.value = data.imdbID;
+            titulo.value = data.Title;
+            year.value = data.Year;
+            types.value = data.Type;
+            poster.value = data.Poster;
+            description.value = data.description;
+            ubicacion.value = data.Ubication;
+            estado.value = Number(data.Estado);
         });
 }
